3f4e069736ff6a5fcd597c9287857056a7f918f4
Sathwik041 <satwikannaldesh@gmail.com>
2026-03-16 21:46:40 +0530

    docs: update metadata title and description

--- packages/nextjs/app/layout.tsx
+++ packages/nextjs/app/layout.tsx
@@ -6,10 +6,9 @@ import "~~/styles/globals.css";
 import { getMetadata } from "~~/utils/scaffold-eth/getMetadata";
 
 export const metadata = getMetadata({
-  title: "Scaffold-ETH 2 App",
+  title: "Supply-onchain",
   description: "Built with 🏗 Scaffold-ETH 2",
 });
-
 const ScaffoldEthApp = ({ children }: { children: React.ReactNode }) => {
   return (
     <html suppressHydrationWarning className={``}>
